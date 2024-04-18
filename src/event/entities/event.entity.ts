2695c6307f3839ae7cf59dee18d81d2ce83a855e
Ni3omi <girasimov1337@gmail.com>
2024-04-18 17:59:32 +0500

    feat(entity):update entity

--- src/event/entities/event.entity.ts
+++ src/event/entities/event.entity.ts
@@ -1,5 +1,7 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { ProjectEntity } from '../../project/entities/project.entity';
+import { EVENT_STATUS } from '@app/event/constants/event.constants';
+import { UserEntity } from '@user/entities/user.entity';
 
 @Entity('events')
 export class EventEntity {
@@ -9,6 +11,16 @@ export class EventEntity {
   @Column()
   name: string;
 
+  @ManyToMany(()=>UserEntity,(user)=>user.id,{ cascade: true })
+  ratedUsersIds:string[]
+
   @OneToMany(()=>ProjectEntity,(project)=>project.event)
-  projects:ProjectEntity[]
+  projects:ProjectEntity[];
+
+  @Column({
+    type: 'enum',
+    enum: EVENT_STATUS,
+    default: EVENT_STATUS[0],
+  })
+  status: typeof EVENT_STATUS[number];
 }
