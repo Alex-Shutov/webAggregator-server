a6a25fd7c728946cfc4449682200240789851cce
Ni3omi <girasimov1337@gmail.com>
2024-04-18 18:03:55 +0500

    del(tags):delete module tags

--- src/tag/tag.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity({ name: 'tags' })
-export class TagEntity {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-  @Column()
-  text:string
-}
