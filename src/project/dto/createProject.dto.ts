55c508596c782850ce6458778e545c15f3e58824
Ni3omi <girasimov1337@gmail.com>
2024-06-15 03:42:43 +0500

    feat(project): add create dto

--- src/project/dto/createProject.dto.ts
+++ src/project/dto/createProject.dto.ts
@@ -1,11 +1,25 @@
 import { PROJECT_STATUSES } from '@app/project/constants/project.constants';
+import { IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProjectDto {
+  @IsOptional()
+  @ApiProperty()
   name:string
+  @IsOptional()
+  @ApiProperty()
   description: string;
+  @IsOptional()
+  @ApiProperty()
   howToPlay: string;
+  @IsOptional()
+  @ApiProperty()
   gitLink: string;
-  eventId:string
-  status:PROJECT_STATUSES.CHECK
+  @ApiProperty()
+  @IsOptional()
+  status:PROJECT_STATUSES = PROJECT_STATUSES.DRAFT
+  @ApiProperty()
+  teamId:string
+
 
 }
